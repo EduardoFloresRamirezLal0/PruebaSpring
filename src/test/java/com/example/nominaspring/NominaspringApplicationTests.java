c4a8c9bf1031f0de111e9938a68e04ea2a75e5bf
Apple <apple@MacBook-Pro-de-Apple-2.local>
2021-09-09 16:06:46 -0500

    Ejemplo de uso de Spring

---
+++ src/test/java/com/example/nominaspring/NominaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nominaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
